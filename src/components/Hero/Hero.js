2fd22d43634750bf61f92efdc2d0e71272086d96
Adi Chondro <gladichondro@gmail.com>
2021-06-18 09:38:51 +0700

    fix navigation

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { IconContext } from "react-icons";
 import { FaMedium } from 'react-icons/fa';
 //import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
