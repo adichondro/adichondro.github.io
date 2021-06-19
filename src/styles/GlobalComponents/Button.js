cbfac7be0b67e6239ebdb22f69703d5e4663c669
Adi Chondro <gladichondro@gmail.com>
2021-06-19 09:34:02 +0700

    fix button

--- src/styles/GlobalComponents/Button.js
+++ src/styles/GlobalComponents/Button.js
@@ -3,10 +3,13 @@ import Link from 'next/link';
 import { ButtonBack, ButtonFront } from './index'
 
 const Button = (props) => (
-  <Link href="https://medium.com/@chondroa" passHref >
-    <ButtonBack target="_blank" alt={props.alt} form={props.form} disabled={props.disabled}>{props.children}
+  <Link href="https://medium.com/@chondroa/" passHref >
+    <ButtonBack target="_blank" rel="noopener noreferrer" alt={props.alt} form={props.form} disabled={props.disabled}>{props.children}
       <ButtonFront alt={props.alt} onClick={props.onClick} disabled={props.disabled}>{props.children}</ButtonFront>
     </ButtonBack>
+    {/* <ButtonBack target="_blank" alt={props.alt} form={props.form} disabled={props.disabled}>{props.children}
+      <ButtonFront alt={props.alt} onClick={props.onClick} disabled={props.disabled}>{props.children}</ButtonFront>
+    </ButtonBack> */}
   </Link>
 
 
